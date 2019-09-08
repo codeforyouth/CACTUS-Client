10f64d6fe7856a817619738e3cfa9bc5762f330f
nakao107107 <imakeanewstory@gmail.com>
2019-09-08 21:23:17 +0900

    tagごとの関連プロジェクトをみられるページを作成

--- pages/language/_language.vue
+++ pages/language/_language.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
-    <section class="bg-warning">
+    <section :style="{backgroundColor: tag.color}">
       <div class="container">
         <div class="row">
           <div class="col-4 p-5">
-            <img class="card-img-top" src="https://www.terakoya.work/wp-content/uploads/2017/04/javascript.jpg" alt="カードの画像">
+            <img class="card-img-top" :src="tag.img_url" alt="カードの画像">
           </div>
-          <div class="col-8 bg-warning p-5 align-middle">
+          <div class="col-8 p-5 align-middle" :style="{backgroundColor: tag.color}">
             <div class="d-flex align-items-center mb-4">
-              <h2 class="font-weight-bold text-white mb-0 mr-2">JavaScript</h2>
+              <h2 class="font-weight-bold text-white mb-0 mr-2">{{tag.name}}</h2>
             </div>
-            <p class="text-white">Webサイトに動きをつけるための言語です。最近ではVueやReactなどのフレームワークを使った開発も盛ん！</p>
+            <p class="text-white">{{tag.description}}</p>
           </div>
         </div>
       </div>
     </section>
     <div class="container mt-3">
-      <h3 class="font-weight-bold mt-5 mb-4">JavaScriptを使ったプロジェクト一覧</h3>
+      <h3 class="font-weight-bold mt-5 mb-4">{{tag.name}}を使ったプロジェクト一覧</h3>
       <div class="row">
-        <project-card :id="1"/>
-        <project-card :id="1"/>
-        <project-card :id="1"/>
-        <project-card :id="1"/>
-        <project-card :id="1"/>
+        <project-card v-for="project in tag.projects" :key="project.id" :project="project"/>
       </div>
     </div>
   </div>
@@ -33,12 +29,32 @@
 
 <script>
   import ProjectCard from '~/components/index/ProjectCard.vue'
+  import { mapGetters } from 'vuex'
 
   export default {
 
     components: {
       ProjectCard
+    },
+
+    async fetch({store, route}){
+
+      try {
+
+        await store.dispatch('tag/init', route.params.language)
+
+      }catch(e){
+
+        console.log("error")
+
+      }
+
+    },
+
+    computed: {
+      ...mapGetters('tag', ['tag'])
     }
+
   }
 
 </script>
