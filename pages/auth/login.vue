e528d6e85e9680c924adad143eaee8b999fed58c
yokinist <ikd618@gmail.com>
2019-10-20 06:36:48 +0900

    :lipstick: Styling login form

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="container d-flex justify-content-center">
-    <button @click="login" class="btn btn-black btn-round"><i class="fab fa-github mr-2"></i>Login with github</button>
+  <div class="container p-5 mt-5">
+    <div class="p-4" />
+    <div class="justify-content-center align-items-center text-center p-5 mt-5">
+      <h1 class="font-weight-bold">🌵 Login to CACTUS 🌵</h1>
+      <button @click="login" class="btn btn-black btn-round btn-lg"><i class="fab fa-github mr-2"></i>Login with github</button>
+    </div>
   </div>
 </template>
 
