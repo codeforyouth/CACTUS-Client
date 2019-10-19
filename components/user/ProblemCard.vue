cf19e8dff16ba6bc7adb7228e7abe1f987f8fc88
nakao107107 <imakeanewstory@gmail.com>
2019-10-19 22:47:17 +0900

    user profileのAPIとの繋ぎこみ

---
+++ components/user/ProblemCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card" @click="goIssuePage">
+    <div class="card-body">
+      <h4 class="card-title font-weight-bold">{{problem.title}}</h4>
+      <p>{{problem.descripton}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    props: [
+      'problem',
+    ],
+
+    methods: {
+      goIssuePage(e){
+        e.preventDefault();
+        this.$router.push( `/issues/${this.problem.id}`)
+      }
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
--- pages/users/_code.vue
+++ pages/users/_code.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="container">
     <div class="d-flex align-items-center mt-4 mb-5">
-      <img src="https://github.com/nakao107107.png" class="rounded-circle mr-2" width="100"></img>
-      <h3 class="text-dark mb-0">nakao107107</h3>
+      <img :src="`https://github.com/${user.github_id}.png`" class="rounded-circle mr-2" width="100"></img>
+      <h3 class="text-dark mb-0">{{user.github_id}}</h3>
     </div>
-    <h3 class="font-weight-bold">作成プロジェクト一覧</h3>
+    <h3 class="font-weight-bold">解決した課題一覧</h3>
     <div class="row">
-      <!--<project-card :id="1"/>-->
-      <!--<project-card :id="1"/>-->
-      <!--<project-card :id="1"/>-->
-      <!--<project-card :id="1"/>-->
-      <!--<project-card :id="1"/>-->
+      <problem-card v-for="solution in user.solutions" :key="solution.id" :problem="solution.problem"/>
     </div>
   </div>
 </template>
 
 <script>
 
-  import ProjectCard from '~/components/index/ProjectCard.vue'
+  import ProblemCard from '~/components/user/ProblemCard.vue'
+  import { mapGetters } from 'vuex'
 
   export default {
 
+    async fetch({store}){
+      await store.dispatch('user/init')
+    },
+
     components: {
-      ProjectCard
+      ProblemCard
+    },
+
+    computed: {
+      ...mapGetters('user', ['user'])
     }
 
   }
