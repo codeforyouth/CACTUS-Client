971024a4ee74574febec9bfe57a0896788f40fa0
yokinist <ikd618@gmail.com>
2019-10-20 06:41:16 +0900

    :fire: Remove unused class

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container p-5 mt-5">
     <div class="p-4" />
-    <div class="justify-content-center align-items-center text-center p-5 mt-5">
+    <div class="text-center p-5 mt-5">
       <h1 class="font-weight-bold">🌵 Login to CACTUS 🌵</h1>
       <button @click="login" class="btn btn-black btn-round btn-lg"><i class="fab fa-github mr-2"></i>Login with github</button>
     </div>
